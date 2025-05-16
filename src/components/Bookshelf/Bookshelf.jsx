d63b3e9bfcee848c96587bb7d36d298a01f5594b
KylieBeck <kylie.beck12@gmail.com>
2025-05-15 18:27:45 -0700

    completed form creation

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+
+    const [newBook, setNewBook] = useState({
+        title: '',
+        author: '',
+    })
+
+
+    const handleInputChange = (event) => {
+        setNewBook({ ...newBook, [event.target.name]: event.target.value });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBooks({ ...books, newBook });
+        setNewBook({
+            title: '',
+            author: '',
+        })
+    }
+
+    console.log("Books", books)
+    console.log("new book", newBook)
+    return (
+
+        <div className="bookshelfDiv">
+            <form onSubmit={handleSubmit}>
+                <div className="formDiv">
+
+                    <h3>Add a Book</h3>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={newBook.title}
+                        onChange={handleInputChange}
+                    />
+                    <label htmlFor="author">Author:</label>
+                    <input
+                        type="text"
+                        id="author"
+                        name="author"
+                        value={newBook.author}
+                        onChange={handleInputChange}
+                    />
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+            <div className="bookCardsDiv">
+                <ul>
+                    {books.map((book) => (
+                        <li key={"b" + book.id}>{book.title}</li>
+                    ))}
+
+                </ul>
+
+
+            </div>
+
+        </div>
+    );
+};
+
+
+export default Bookshelf;
